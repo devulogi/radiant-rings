7a532e5fa38c5e310061dae9110ddeaa06312b8c
devulogi <devulogijrp.github@gmail.com>
2023-12-06 02:37:10 +0800

    feat: add schema files for cart, address, wishlist, order, and review [5c755fd]

---
+++ models/schemas/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  cartSchema: require('./cart.schema'),
+  addressSchema: require('./address.schema'),
+  wishlistSchema: require('./wishlist.schema'),
+  orderSchema: require('./order.schema'),
+  reviewSchema: require('./review.schema')
+};
